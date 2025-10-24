5081338dfcd6688800289ab8afab2be93469a9ac
ltakoushi18 <ltakoushi18@gmail.com>
2025-10-24 17:28:58 +1000

    fixed bug with css on output

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function displayFilmQuote(response) {
-  new Typewriter("#film-quote-actual", {
+  new Typewriter("#insert-quote", {
     strings: response.data.answer,
     autoStart: true,
     delay: 1,
@@ -13,11 +13,11 @@ function generateFilmQuote(event) {
   let instructions = document.querySelector("#user-instructions");
   let apiKey = "359oa204a337et1ba70bbe4fd2e2f596";
   let context =
-    "You are a film expert that knows quotes from lots of films. Your mission is to generate just the well-known quote from the chosen film. Make sure the follow user instructions";
+    "You are a film expert that knows quotes from lots of films. Your mission is to generate just the well-known quote from the chosen film. Make sure to follow user instructions";
   let prompt = `User instructions: Generate a film quote from ${instructions.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  let quoteHidden = document.querySelector("#film-quote-actual");
+  let quoteHidden = document.querySelector("#insert-quote");
   quoteHidden.classList.remove("hidden");
   quoteHidden.innerHTML = "💬 Generating...";
   axios.get(apiUrl).then(displayFilmQuote);
